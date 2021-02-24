067fd6b4132c182f2419e5267f37e43ed220d385
speeldesign <m.hokamura@speel.design>
2021-02-24 16:45:09 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Masaru Hokamura on 2021/02/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
