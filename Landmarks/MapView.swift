a59b8196a36b0158b8792a89096995d52c799bd6
speeldesign <m.hokamura@speel.design>
2021-02-26 18:34:59 +0900

    1 Section 5-4

---
+++ Landmarks/MapView.swift
@@ -0,0 +1,26 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by Masaru Hokamura on 2021/02/26.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
+        span: MKCoordinateSpan(latitudeDelta: 1.0, longitudeDelta: 1.0)
+    )
+    
+    var body: some View {
+        Map(coordinateRegion: $region)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
